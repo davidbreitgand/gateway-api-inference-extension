3a163a6cc25d93bb4429ff0e235fe1039aedfbb1
David Breitgand <davidbreitgand@users.noreply.github.com>
2026-01-11 18:36:27 +0200

    Interfaces towards pluggable BBR framework (initial PR). Current BBR behavior unchanged.

---
+++ pkg/bbr/plugins/handle.go
@@ -0,0 +1,71 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package plugins
+
+import (
+	"context"
+)
+
+// HandlePlugin defines a set of APIs to work with an instantiated plugin
+type HandlePlugin interface {
+	// AddPlugin adds a plugin
+	AddBBRPlugin(name string, plugin BBRPlugin)
+
+	// GetBBRPlugin returns a BBRPlugin instance by name
+	GetBBRPlugin(name string) BBRPlugin
+}
+
+// Handle provides a standard set of tools to the plugin instance
+type Handle interface {
+	HandlePlugin
+	Context() context.Context
+}
+
+// bbrHandle implements HandlePlugin interface
+type bbrHandle struct {
+	ctx context.Context
+	HandlePlugin
+}
+
+// Context returns a context that might be required by a plugin
+func (h *bbrHandle) Context() context.Context {
+	return h.ctx
+}
+
+// bbrHandlePlugin implements HandlePlugin APIs to work with an instantiated plugin
+type bbrHandlePlugin struct {
+	plugin BBRPlugin
+}
+
+// AddBBRPlugin adds an instance of BBRPlugin. In this implementation, only one plugin instance can be added.
+func (h *bbrHandlePlugin) AddBBRPlugin(name string, plugin BBRPlugin) {
+	h.plugin = plugin
+}
+
+// GetBBRPlugin gets an instance of BBRPlugin
+// The caller should always check for nil. There is no validly nil condition.
+func (h *bbrHandlePlugin) GetBBRPlugin(name string) BBRPlugin {
+	return h.plugin
+}
+
+// Constructor for bbrHandle
+func NewBbrHandle(ctx context.Context) Handle {
+	return &bbrHandle{
+		ctx:          ctx,
+		HandlePlugin: &bbrHandlePlugin{},
+	}
+}
---
+++ pkg/bbr/plugins/plugins.go
@@ -0,0 +1,43 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package plugins
+
+import (
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/plugins"
+)
+
+const (
+	ModelHeader     = "X-Gateway-Model-Name"
+	BaseModelHeader = "X-Gateway-Base-Model-Name"
+)
+
+type BBRPlugin interface {
+	plugins.Plugin
+
+	// Execute runs the plugin logic on the request body and a map of headers.
+	// A plugin's imnplementation logic CAN mutate the body of the message.
+	// A plugin's implementation MUST return a map of headers.
+	// If no headers are set by the implementation, the return headers map is nil.
+	// A value of a header in an extended implementation NEED NOT to be identical to the value of that same header as would be set
+	// in a default implementation.
+	// Example: in the body of a request model is set to "semantic-model-selector",
+	// which, say, stands for "select a best model for this request at minimal cost"
+	// A plugin implementation of "semantic-model-selector" sets X-Gateway-Model-Name to any valid
+	// model name from the inventory of the backend models and also mutates the body accordingly
+	// If the body is not mutated, the original request body MUST be returned.
+	SetHeadersAndBody(requestBodyBytes []byte, requestHeaders map[string][]string) (headers map[string][]string, mutatedBodyBytes []byte, err error)
+}
